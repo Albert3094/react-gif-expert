2dc93b01b06a889fea711298a37406ab7e606e10
Albert <albert.aguilera.bague@gmail.com>
2024-12-19 10:15:35 +0100

    first commit

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,26 @@
+import { GifItem } from './GridItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+
+//rafc tab
+export const GifGrid = ({category}) => {
+
+  const { images, isLoading } = useFetchGifs ( category );
+  
+  return (
+    <>
+        <h3>{ category }</h3>
+        {
+          isLoading && (<h2>Cargando...</h2>)
+        }
+        <div className='card-grid'>
+          {
+            images.map((image) => (
+              <GifItem 
+                key={image.id} 
+                { ...image }
+              />))
+          }
+        </div>
+    </>
+  )
+}
